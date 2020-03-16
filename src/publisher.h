413e17f696a94c719f3c2cb10ff8f0b3ed989255
twanas <antoine@reltech.com>
2020-03-16 14:47:14 +1100

    Added filtering on symbol groups

--- src/publisher.h
+++ src/publisher.h
@@ -3,7 +3,8 @@
 #include "connection.h"
 #include "OnixS/CME/MDH.h"
 #include <memory>
-#include <array>
+#include <vector>
+#include <unordered_map>
 #include <sstream>
 
 using namespace OnixS::CME::MDH;
@@ -14,6 +15,7 @@ void initialize() {
     OnixS::CME::MDH::initialize(settings);
 }
 
+using SecurityGroupList = std::vector<std::string>;
 
 class Publisher : SecurityListener, MarketDataListener
 {
@@ -22,6 +24,20 @@ public:
             : subject_(subject)
               , conn_(conn) {
 
+        std::unordered_map<ChannelId, SecurityGroupList> channels {
+                {310, {"ES"}},
+                {312, {"GE"}},
+                {314, {"6A", "6B", "6J", "6S"}},
+                {318, {"N1", "NQ", "RY"}},
+                {320, {"6C", "6E"}},
+                {340, {"ZC", "ZW"}},
+                {342, {"YM"}},
+                {344, {"ZB", "ZF", "ZN", "ZT", "ZU"}},
+                {346, {"ZS"}},
+                {360, {"GC", "HG", "SI"}},
+                {382, {"CL"}}
+        };
+
         FileLoggerSettings filelogger_cfg;
         filelogger_cfg.severityLevel(LogSeverity::Enum::Important);
         filelogger_cfg.filename("FeedHandler.log");
@@ -29,24 +45,8 @@ public:
 
         FeedEngineSettings engine_cfg;
         engine_cfg.socketBufferSize(8 * 1024 * 1024);
-        engine_cfg.threadCount(8);
+        engine_cfg.threadCount(channels.size());
         engine_ = std::unique_ptr<FeedEngine>(new FeedEngine(engine_cfg));
-
-        std::vector<int> channels{
-                310,
-                312,
-                314,
-                318,
-                320,
-                340,
-                342,
-                344,
-                346,
-                360,
-                382,
-                384,
-                386
-        };
 
         for (auto channel: channels) {
             HandlerSettings handler_cfg;
@@ -54,23 +54,29 @@ public:
             handler_cfg.bookManagement().consolidatedBooks().maintain(false);
             handler_cfg.bookManagement().mboBooks().maintain(false);
             handler_cfg.feeds().connectivityConfigurationFile(CONNECTIVITY_CONFIGURATION_FILE);
-            handler_cfg.channel(channel);
+            handler_cfg.channel(channel.first);
             handler_cfg.feeds().feedANetworkInterfaces(NETWORK_INTERFACE_A);
             handler_cfg.feeds().feedBNetworkInterfaces(NETWORK_INTERFACE_B);
-          
+
             auto handler = std::unique_ptr<Handler>(new Handler(handler_cfg));
             handler->bindFeedEngine(*engine_);
             handler->bindLogger(*file_logger_);
             handler->registerSecurityListener(*this);
             handler->registerMarketDataListener(*this);
 
-	    handlers_.push_back(std::move(handler));
+            InstrumentSelection filter;
+            for (auto &sec_group: channel.second) {
+                filter.securityGroups().insert(toStrRef(sec_group));
+            }
+            handler->selectInstruments(filter);
+
+            handlers_.push_back(std::move(handler));
         }
 
-       for (auto& handler: handlers_) {
+        for (auto &handler: handlers_) {
             SessionSettings session_cfg;
             handler->start(session_cfg);
-       }
+        }
     }
 
     void onLimitsAndBanding(Handler &, const Security &sec, const LimitsAndBanding50Args &args) override {
@@ -95,7 +101,7 @@ public:
         handleLimitsAndBanding(sec, args.message());
     }
 
-    void onPacket(Handler &, const PacketArgs & packet) override {
+    void onPacket(Handler &, const PacketArgs &packet) override {
         sendingtime_ = packet.sendingTime();
     }
 
@@ -104,27 +110,29 @@ private:
     template<typename Entry>
     void handleLimitsAndBanding(const Security &sec, const Entry &entry) {
 
-	bool high_or_low{false};
+        bool high_or_low{false};
 
         std::stringstream ss;
+//        ss.clear();
+
         ss << "{\"sendingtime\": \"" << toStr(sendingtime_) << "\"";
         ss << ", \"symbol\": \"" << sec.symbol() << "\"";
 
         Decimal high, low;
         if (entry.highLimitPrice(high)) {
-    	   ss << ", \"high_limit\": " << toStr(high);
-	   high_or_low = true;
-	}
+            ss << ", \"high_limit\": " << toStr(high);
+            high_or_low = true;
+        }
         if (entry.lowLimitPrice(low)) {
-	   ss << ", \"low_limit\": " << toStr(low);
-	   high_or_low = true;
-	}
+            ss << ", \"low_limit\": " << toStr(low);
+            high_or_low = true;
+        }
 
-	ss << "}";
+        ss << "}";
 
-	if(high_or_low) {
-	    conn_->publish("cme.md", ss.str());
-	}
+        if (high_or_low) {
+            conn_->publish("cme.md", ss.str());
+        }
 
     }
 
