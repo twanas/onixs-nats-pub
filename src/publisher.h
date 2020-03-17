f1eb3a4574b222634c5f28f2b501b983c2ca3e99
twanas <antoine@reltech.com>
2020-03-18 09:24:49 +1100

    Fixed bug where we mis-labled updates on onLimitsAndBanding

--- src/publisher.h
+++ src/publisher.h
@@ -40,9 +40,9 @@ public:
 //        };
 
         std::unordered_map<ChannelId, SecurityGroupList> channels{
-                {310, { "ESH1", "ESH0", "ESU0", "ESZ0", "ESM0" }},
-                {342, { "YMZ0", "YMM0", "YMU0", "YMH0" }},
-                {318, { "NIYM2", "NIYU1", "NIYN0", "NIYJ0", "NIYK0", "NIYG0", "NIYM0", "NIYU0", "NIYU2", "NIYM1", "NIYZ1", "NIYZ0", "NIYZ5", "NIYH2", "NIYH0", "NIYH1", "RTYH1", "NQH1", "NQH0", "RTYH0", "RTYU0", "NQU0", "RTYZ0", "NQZ0", "NQM0", "RTYM0", "NIYZ3", "NIYZ4", "NIYZ2" }}
+                {310, {"ESH1",  "ESH0",  "ESU0",  "ESZ0",  "ESM0"}},
+                {342, {"YMZ0",  "YMM0",  "YMU0",  "YMH0"}},
+                {318, {"NIYM2", "NIYU1", "NIYN0", "NIYJ0", "NIYK0", "NIYG0", "NIYM0", "NIYU0", "NIYU2", "NIYM1", "NIYZ1", "NIYZ0", "NIYZ5", "NIYH2", "NIYH0", "NIYH1", "RTYH1", "NQH1", "NQH0", "RTYH0", "RTYU0", "NQU0", "RTYZ0", "NQZ0", "NQM0", "RTYM0", "NIYZ3", "NIYZ4", "NIYZ2"}}
 //                {340, { "ZCK2", "ZWK0", "ZWN1", "ZWH1", "ZCU2", "ZCU1", "ZCN1", "ZWZ1", "ZCH0", "ZCN3", "ZWH0", "ZCK1", "ZCH2", "ZWU0", "ZWZ0", "ZCZ0", "ZCZ1", "ZWU1", "ZWN0", "ZCH1", "ZWK2", "ZCK0", "ZCN0", "ZCZ2", "ZWK1", "ZCZ3", "ZWH2", "ZWN2", "ZCU0", "ZCN2" }},
 //                {320, { "6CZ4", "6CN0", "6CZ0", "6CH4", "6CG0", "6CM1", "6CZ1", "6CU4", "6CK0", "6CU3", "6CZ2", "6EH1", "6CZ3", "6CU0", "6EG0", "6CJ0", "6EK0", "6CH1", "6CU2", "6EJ0", "6CH3", "6EM2", "6EM3", "6EN0", "6CU1", "6CH2", "6CH0", "6EH2", "6CM4", "6CM0", "6EU2", "6EH0", "6EM0", "6EM1", "6EZ1", "6EZ0", "6EU0", "6EM4", "6EZ4", "6CM3", "6EH4", "6EU3", "6EZ2", "6CM2", "6EU4", "6EU1", "6EH3", "6EZ3" }},
 //                {382, { "CLK0", "CLQ4", "CLH5", "CLF5", "CLQ30", "CLM3", "CLU5", "HOU0", "RBQ3", "RBJ3", "RBZ3", "CLF1", "CLZ30", "CLV3", "CLF4", "HOH3", "CLF3", "CLN0", "CLK4", "HOK1", "HON1", "HOK3", "CLN7", "CLK2", "CLM5", "CLG2", "RBX0", "RBX1", "HOF2", "RBM1", "CLX7", "HOM2", "CLK6", "CLH1", "CLH8", "CLV8", "CLX8", "CLN9", "CLU9", "CLG30", "RBJ0", "CLX1", "CLH7", "HOG2", "CLH6", "CLN4", "RBN2", "RBK2", "RBV2", "RBZ2", "HOQ0", "CLZ0", "CLQ5", "CLQ9", "RBQ0", "CLZ3", "HOU2", "HOZ0", "CLN30", "CLX4", "CLJ8", "CLX0", "CLH2", "CLF6", "RBX3", "CLF30", "CLX2", "HOF3", "RBV1", "HOF1", "CLM1", "CLG3", "CLZ4", "CLX30", "CLN8", "CLU3", "CLQ2", "RBK1", "CLJ4", "RBM3", "HOV3", "CLK3", "CLJ9", "CLV0", "CLQ1", "RBU1", "CLX6", "RBF2", "HOJ1", "HOZ1", "HOJ3", "CLM4", "CLM9", "RBZ0", "RBX2", "CLM2", "CLG1", "CLQ6", "HON0", "CLG31", "HOQ3", "CLV7", "CLG6", "HOX0", "CLV4", "CLN5", "RBV3", "CLV9", "RBN0", "CLZ7", "HOK2", "HOJ0", "CLZ1", "RBJ2", "CLK5", "CLN2", "CLX5", "RBN3", "CLZ9", "CLV2", "CLF8", "HOZ2", "RBH0", "RBJ1", "CLZ6", "CLM30", "CLK8", "CLF2", "CLH4", "RBZ1", "HOH1", "RBH3", "HOQ2", "HOU3", "CLJ3", "CLV30", "CLQ8", "CLQ3", "CLU0", "CLN1", "RBQ1", "CLF7", "CLZ2", "CLJ0", "HOV1", "HOX1", "RBU2", "CLK9", "HOM1", "RBM2", "HOF4", "CLH9", "CLK1", "CLU6", "RBV0", "RBG1", "CLH0", "HOM0", "CLK7", "CLJ30", "RBM0", "HOM3", "CLU7", "HOJ2", "HOV0", "CLV1", "CLU4", "RBU3", "CLG9", "HOX2", "HOH0", "RBH2", "CLG5", "CLJ2", "CLJ5", "RBK3", "CLX9", "CLG8", "HON2", "RBH1", "CLK30", "CLM8", "CLZ5", "CLM0", "RBN1", "CLV5", "HOG1", "RBG3", "CLU2", "RBF4", "HOX3", "CLM6", "CLU30", "CLU8", "CLN3", "CLG4", "HOU1", "RBQ2", "HOZ3", "CLH3", "CLF31", "CLF9", "CLX3", "CLV6", "RBF3", "HOQ1", "HON3", "CLQ7", "CLZ8", "CLJ1", "HOG3", "RBU0", "CLH30", "RBK0", "CLM7", "HOH2", "CLJ6", "RBG2", "CLU1", "CLN6", "HOK0", "RBF1", "CLJ7", "HOV2", "CLG7", "CLQ0"}},
@@ -97,17 +97,11 @@ public:
     }
 
     void onLimitsAndBanding(Handler &, const Security &sec, const LimitsAndBanding50Args &args) override {
-        auto &msg = args.message();
-        for (auto &entry: msg.entries()) {
-            handleLimitsAndBanding(sec, entry, args.message().transactTime());
-        }
+        handleLimitsAndBanding(sec, args.entry(), args.message().transactTime());
     }
 
     void onLimitsAndBanding(Handler &, const Security &sec, const LimitsAndBanding34Args &args) override {
-        auto &msg = args.message();
-        for (auto &entry: msg.entries()) {
-            handleLimitsAndBanding(sec, entry, args.message().transactTime());
-        }
+        handleLimitsAndBanding(sec, args.entry(), args.message().transactTime());
     }
 
     void onRecovery(Handler &, const Security &sec, const SnapshotFullRefresh38Args &args) override {
@@ -125,7 +119,7 @@ public:
 
 private:
     template<typename Entry>
-    void handleLimitsAndBanding(const Security &sec, const Entry &entry, const Timestamp& transacttime) {
+    void handleLimitsAndBanding(const Security &sec, const Entry &entry, const Timestamp &transacttime) {
 
         std::lock_guard<std::mutex> lock(mtx_);
 
